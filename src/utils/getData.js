852dadc23499330670e67e569a19d95e0f461283
Felipe <fquiceno1@misena.edu.co>
2022-04-20 10:04:05 -0500

    Creamos la estructura basica para realizar peticiones a la API con async y await

---
+++ src/utils/getData.js
@@ -0,0 +1,13 @@
+const API = 'https://rickandmortyapi.com/api/character'
+
+const getData = async(id) =>{
+    const apiURL = id ? `${API}${id}` : API;    //Si el id existe, contatena ese id a la url si no devuelve solo la url
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        return data;
+    }catch (error) {
+        console.log('Fetch Error', error)
+    };
+};
+export default getData;
