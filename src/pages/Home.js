5cc2f7de69549e0d952c3806bbfb4e30aafcc7a4
Felipe <fquiceno1@misena.edu.co>
2022-04-19 23:37:15 -0500

    Se crea funcion en Home

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+const Home = () => {
+
+    /*
+        Creamos la estructura de html para los characters
+    */
+    const view =/*HTML*/ `
+        <div class="Characters">
+            <article class="Charachter-item">
+            <a href="#/1/">
+                <img src="image alt="name">
+                <h2>Name</h2>
+            </a>
+            </article>
+        </div>
+    `;
+    return view;
+}
+
+export default Home;
