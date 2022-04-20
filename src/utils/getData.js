52537eb5ca7f96b3b84b4cb485878b0e5bcb7447
Felipe <fquiceno1@misena.edu.co>
2022-04-20 10:07:05 -0500

    Convertimos el Home en un async y await ya que hará uso de la funcion getData() e importamos este archivo

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -2,6 +2,7 @@ const API = 'https://rickandmortyapi.com/api/character'
 
 const getData = async(id) =>{
     const apiURL = id ? `${API}${id}` : API;    //Si el id existe, contatena ese id a la url si no devuelve solo la url
+    
     try {
         const response = await fetch(apiURL);
         const data = await response.json();
