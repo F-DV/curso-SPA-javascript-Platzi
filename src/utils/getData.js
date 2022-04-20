630612c8890a8aae8e4c39cedb8999e7abfab36f
Felipe <fquiceno1@misena.edu.co>
2022-04-20 10:37:44 -0500

    Le damos la estructura de character para mostrar las caracteristicas de cada personaje, en este caso no iteras ya que solo mostrara la informacion del 1 solo personage

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,4 +1,4 @@
-const API = 'https://rickandmortyapi.com/api/character'
+const API = 'https://rickandmortyapi.com/api/character/'
 
 const getData = async(id) =>{
     const apiURL = id ? `${API}${id}` : API;    //Si el id existe, contatena ese id a la url si no devuelve solo la url
