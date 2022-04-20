267a620480d0a1b1daf5fa6ac7bd7b0dacc7087f
Felipe <fquiceno1@misena.edu.co>
2022-04-20 08:45:52 -0500

    Creamos el archivo resolveRoutes para la validacion de las rutas

--- src/utils/getHash.js
+++ src/utils/getHash.js
@@ -3,4 +3,7 @@ location.hash.slice(1)
 .toLocaleLowerCase()    //Conviertelo a minuscula
 .split('/')[1] || '/';  //retira los '/'. y dame el elemento que necesito en este caso el de la posicion 1 ['', '1', '']
 
+/*
+    nos exporta un id o un '/' que evaluaremos en resolveRoutes
+*/
 export default getHash;
---
+++ src/utils/resolveRoutes.js
@@ -0,0 +1,11 @@
+const resolveRoutes = (route) => {
+    
+    if(route.length <= 3){          //Evaluamos que no tenga mas de 3 caracteres ya que no tenemos mas de 1000 personajes
+
+        let validRoute = route === '/' ? route : '/:id';
+        return validRoute;
+    }       
+    return `/${route}`;     // si no devuelve route que seria la misma que ingreso
+};
+
+export default resolveRoutes;
